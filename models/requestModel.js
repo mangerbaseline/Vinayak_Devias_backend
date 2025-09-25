0bf130d0a9b1af9059cb69fc319e6d575c66f28f
Vinayak <manageratbaseline@gmail.com>
2025-09-25 12:07:11 +0530

    updated code

--- models/requestModel.js
+++ models/requestModel.js
@@ -1,20 +1,50 @@
+// const mongoose = require('mongoose');
+
+// const requestSchema = new mongoose.Schema(
+//   {
+//     senderId: [
+//       {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'User',
+//         required: true
+//       }
+//     ],
+
+//     receiverId: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: 'User',
+//       required: true
+//     },
+//     status: {
+//       type: String,
+//       enum: ['pending', 'accepted', 'rejected'],
+//       default: 'pending',
+//     },
+//   },
+//   { timestamps: true }
+// );
+
+// module.exports = mongoose.model('Request', requestSchema);
+
+
+
+
 const mongoose = require('mongoose');
 
 const requestSchema = new mongoose.Schema(
   {
-    senderId: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-      }
-    ],
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
 
     receiverId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
       required: true
     },
+
     status: {
       type: String,
       enum: ['pending', 'accepted', 'rejected'],
